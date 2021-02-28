e72de25e11a32ab6f3eccf264430b2683a936148
Timsalabim1991 <71948868+Timsalabim1991@users.noreply.github.com>
2021-02-28 23:17:13 +0100

    Update question-2.js

--- question-2.js
+++ question-2.js
@@ -1,1 +1,36 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
+const resultsContainer = document.querySelector(".results");
+
+async function getApi() {
+    // fetch
+
+    const response = await fetch(url);
+
+    const results = await response.json();
+
+    const facts = results.results;
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < facts.length; i++) {
+		console.log(facts[i].name);
+		
+		if (i === 8) {
+            break;
+        }
+
+	    for (let i = 0; i < facts.length; i++) {
+			console.log(facts[i].name);
+
+        if (i === 8) {
+            break;
+        }
+	}
+		resultsContainer.innerHTML += `<div class="result">${facts[i].name}</div>`;
+		
+		resultsContainer.innerHTML += `<div class="result">${facts[i].rating}</div>`;
+    }
+}
+
+getApi();
